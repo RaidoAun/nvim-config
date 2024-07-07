15f4155019605d55d944a44b28ef64020e5ffb3a
RaidoAun <45874591+RaidoAun@users.noreply.github.com>
2024-07-07 23:56:00 +0300

    use tabs instead of spaces

--- lua/config/opts.lua
+++ lua/config/opts.lua
@@ -15,7 +15,7 @@ vim.opt.wrap = false
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
+vim.opt.expandtab = false
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
 
