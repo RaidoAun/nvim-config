809de5586a28011aa7c9b17583e9ae6c5d4b00d9
RaidoAun <45874591+RaidoAun@users.noreply.github.com>
2024-09-27 00:54:17 +0300

    add telescope.jumplist

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -61,6 +61,7 @@ return {  -- Fuzzy Finder (files, lsp, etc)
 		vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
 		vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
 		vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+		vim.keymap.set('n', '<leader>jl', builtin.jumplist, { desc = '[J]ump [L]ist' })
 
 		-- Slightly advanced example of overriding default behavior and theme
 		-- vim.keymap.set('n', '<leader>/', function()
