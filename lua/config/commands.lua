52961c0e894bab24b885ca396f90a48291e97faa
RaidoAun <45874591+RaidoAun@users.noreply.github.com>
2024-09-26 21:49:13 +0300

    fix GitPrune command

--- lua/config/commands.lua
+++ lua/config/commands.lua
@@ -2,7 +2,7 @@ vim.api.nvim_create_user_command(
   'GitPrune',
   function()
     vim.cmd('!git fetch --prune')
-    vim.cmd('!git branch -vv | grep ": gone]" | awk "{print $1}" | xargs -r git branch -d')
+    vim.cmd('!git branch -vv | grep ": gone]" | awk \'{print $1}\' | xargs -r git branch -d')
   end,
   {}
 )
