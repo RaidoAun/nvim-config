012cd4bea27c5c3684a9eba3981dea698b3e7c35
RaidoAun <45874591+RaidoAun@users.noreply.github.com>
2025-09-09 00:50:13 +0300

    tidy lsp config

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -41,84 +41,86 @@ return { -- LSP Configuration & Plugins
 		--    That is to say, every time a new file is opened that is associated with
 		--    an lsp (for example, opening `main.rs` is associated with `rust_analyzer`) this
 		--    function will be executed to configure the current buffer
+		
 		vim.api.nvim_create_autocmd("LspAttach", {
 			group = vim.api.nvim_create_augroup("kickstart-lsp-attach", { clear = true }),
-			callback = function(event)
+			callback = function(attachEvent)
 				-- NOTE: Remember that Lua is a real programming language, and as such it is possible
 				-- to define small helper and utility functions so you don't have to repeat yourself.
 				--
 				-- In this case, we create a function that lets us more easily define mappings specific
 				-- for LSP related items. It sets the mode, buffer and description for us each time.
-				local map = function(keys, func, desc)
-					vim.keymap.set("n", keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
+				local map = function(mode, keys, func, desc)
+					vim.keymap.set(mode, keys, func, { buffer = attachEvent.buf, desc = "LSP: " .. desc, noremap = true})
 				end
 
 				-- Jump to the definition of the word under your cursor.
 				--  This is where a variable was first declared, or where a function is defined, etc.
 				--  To jump back, press <C-t>.
-				map("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
+				map("n", "gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
 
 				-- Find references for the word under your cursor.
-				map("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
+				map("n", "gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
 
 				-- Jump to the implementation of the word under your cursor.
 				--  Useful when your language has ways of declaring types without an actual implementation.
-				map("gI", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
+				map("n", "gI", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
 
 				-- Jump to the type of the word under your cursor.
 				--  Useful when you're not sure what type a variable is and you want to see
 				--  the definition of its *type*, not where it was *defined*.
-				map("<leader>D", require("telescope.builtin").lsp_type_definitions, "Type [D]efinition")
+				map("n", "<leader>D", require("telescope.builtin").lsp_type_definitions, "Type [D]efinition")
 
 				-- Fuzzy find all the symbols in your current document.
 				--  Symbols are things like variables, functions, types, etc.
-				map("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
+				map("n", "<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
 
 				-- Fuzzy find all the symbols in your current workspace.
 				--  Similar to document symbols, except searches over your entire project.
-				map("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
+				map("n", "<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
 
 				-- Rename the variable under your cursor.
 				--  Most Language Servers support renaming across files, etc.
-				map("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
+				map("n", "<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
 
 				-- Execute a code action, usually your cursor needs to be on top of an error
 				-- or a suggestion from your LSP for this to activate.
-				map("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
+				map("n", "<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
 
 				-- Opens a popup that displays documentation about the word under your cursor
 				--  See `:help K` for why this keymap.
-				map("K", vim.lsp.buf.hover, "Hover Documentation")
+				map("n", "K", function() vim.lsp.buf.hover({border = "rounded"}) end, "Hover Documentation")
+				map("n", "<C-k>", function() vim.lsp.buf.signature_help({border = "rounded", close_events = { "CursorMoved", "BufHidden" }}) end, "Signature help")
 
 				-- WARN: This is not Goto Definition, this is Goto Declaration.
 				--  For example, in C this would take you to the header.
-				map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
+				map("n", "gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
 
 				-- The following two autocommands are used to highlight references of the
 				-- word under your cursor when your cursor rests there for a little while.
 				--    See `:help CursorHold` for information about when this is executed
 				--
 				-- When you move your cursor, the highlights will be cleared (the second autocommand).
-				local client = vim.lsp.get_client_by_id(event.data.client_id)
+				local client = vim.lsp.get_client_by_id(attachEvent.data.client_id)
 				if client and client.server_capabilities.documentHighlightProvider then
 					local highlight_augroup = vim.api.nvim_create_augroup("kickstart-lsp-highlight", { clear = false })
 					vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
-						buffer = event.buf,
+						buffer = attachEvent.buf,
 						group = highlight_augroup,
 						callback = vim.lsp.buf.document_highlight,
 					})
 
 					vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
-						buffer = event.buf,
+						buffer = attachEvent.buf,
 						group = highlight_augroup,
 						callback = vim.lsp.buf.clear_references,
 					})
 
 					vim.api.nvim_create_autocmd("LspDetach", {
 						group = vim.api.nvim_create_augroup("kickstart-lsp-detach", { clear = true }),
-						callback = function(event2)
+						callback = function(attachEvent2)
 							vim.lsp.buf.clear_references()
-							vim.api.nvim_clear_autocmds({ group = "kickstart-lsp-highlight", buffer = event2.buf })
+							vim.api.nvim_clear_autocmds({ group = "kickstart-lsp-highlight", buffer = attachEvent2.buf })
 						end,
 					})
 				end
@@ -126,14 +128,14 @@ return { -- LSP Configuration & Plugins
 					local auto_format_augroup = vim.api.nvim_create_augroup("lsp-auto-format", { clear = false })
 					vim.api.nvim_create_autocmd("BufWritePre", {
 						group = auto_format_augroup,
-						buffer = event.buf,
+						buffer = attachEvent.buf,
 						callback = function()
 							vim.lsp.buf.format()
 						end,
 					})
 					vim.api.nvim_create_autocmd("LspDetach", {
-						callback = function(event2)
-							vim.api.nvim_clear_autocmds({ group = auto_format_augroup, buffer = event2.buf })
+						callback = function(detachEvent)
+							vim.api.nvim_clear_autocmds({ group = auto_format_augroup, buffer = detachEvent.buf })
 						end,
 					})
 				end
@@ -144,53 +146,22 @@ return { -- LSP Configuration & Plugins
 		--  By default, Neovim doesn't support everything that is in the LSP specification.
 		--  When you add nvim-cmp, luasnip, etc. Neovim now has *more* capabilities.
 		--  So, we create new capabilities with nvim cmp, and then broadcast that to the servers.
-		local capabilities = vim.lsp.protocol.make_client_capabilities()
-		capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
-
-		local function is_executable(name)
-			return vim.fn.executable(name) == 1
-		end
-
-		local lspconfig = require("lspconfig")
-
-		if is_executable("zls") then
-			lspconfig.zls.setup({})
-		end
-
-		-- if is_executable("clangd-18") then
-		--   lspconfig.clangd.setup{
-		-- 	cmd = { "clangd-18" }
-		--   }
-		-- end
-		--
-		-- if is_executable("ccls") then
-		-- 	lspconfig.ccls.setup {
-		-- 	  init_options = {
-		-- 		cache = {
-		-- 		  directory = ".ccls-cache";
-		-- 		};
-		-- 	  },
-		-- 	  filetypes = { "c" , "h"}
-		-- 	}
-		-- end
-
-		if is_executable("gopls") then
-			lspconfig.gopls.setup({
+		local servers = {
+			zls = {},
+			ccls = {},
+			gopls = {
 				settings = {
 					gopls = {
 						analyses = {
 							unusedparams = true,
 						},
 						staticcheck = true,
-						gofumpt = true,
+						gofumpt = false,
 						vulncheck = "Imports",
 					},
 				},
-			})
-		end
-
-		if is_executable("lua-language-server") then
-			lspconfig.lua_ls.setup({
+			},
+			lua_ls = {
 				on_init = function(client)
 					if client.workspace_folders then
 						local path = client.workspace_folders[1].name
@@ -222,7 +193,28 @@ return { -- LSP Configuration & Plugins
 				settings = {
 					Lua = {}
 				}
-			})
+			}
+		}
+
+
+		local function is_executable(name)
+			return vim.fn.executable(name) == 1
+		end
+
+
+		local capabilities = vim.lsp.protocol.make_client_capabilities()
+		capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
+		local lspconfig = require("lspconfig")
+
+		for server_name, server_config in pairs(servers) do
+			if is_executable(server_name) then
+				-- Optional: Add on_attach, capabilities if you use them elsewhere
+				-- server_config.on_attach = on_attach -- if defined
+				server_config.capabilities = capabilities -- if defined
+
+				-- Setup the server
+				lspconfig[server_name].setup(server_config)
+			end
 		end
 	end,
 }
